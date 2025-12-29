8fba84e5ad61646887de5b547717146fb95e97d3
Truman Yen <yen.truman@gmail.com>
2025-12-29 15:50:58 -0700

    Add debugs to dummy presenter
    for temporary testing purposes

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ from harmony_analyzer import HarmonyAnalyzer
 
 class DummyHarmonyPresenter(I_HarmonyPresenter):
     def update_harmony_state(self, state: HarmonyState):
-        pass
+        current_chord = state.current_chord
+        if current_chord:
+            note_name = current_chord.root.note_name.name
+            note_accidental = "#" if current_chord.root.accidentals == 1 else ""
+            print(
+                f"Current chord: {note_name}{note_accidental} {current_chord.chord_type.name}"
+            )
 
     def run_ui_until_stopped_by_user(self):
         pass
