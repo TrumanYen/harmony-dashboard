37dee74998f0496773d7938458370c4f40c1bfc8
Truman Yen <yen.truman@gmail.com>
2025-12-29 18:33:12 -0700

    Create scale analyzer
    jist of the algorithm:
    - represent recently detected chords in a 2d array where rows are the chord type
      and columns are the root note (wrapped between A and G#)
    - each key center can be represented as a kernel (reward certain chords like
      tonic, dominant, submediant, while punishing other chords that don't fit)
    - you can convolve this kernel across your 2d array and the position with the
      highest value represents the most likely key center
    
    tl;dr machine learning in the sense that I told the machine what to think
          and it learned to better f*cking listen to me.

--- scale_analyzer.py
+++ scale_analyzer.py
@@ -1,4 +1,13 @@
-from harmony_domain import NoteName, Note, ChordType, Chord, HarmonyState
+import numpy as np
+
+from harmony_domain import (
+    NoteName,
+    Note,
+    ChordType,
+    Chord,
+    HarmonyState,
+    ScaleAgnosticChord,
+)
 
 
 class ScaleAnalyzer:
@@ -21,3 +30,52 @@ class ScaleAnalyzer:
 
     def map_wrapped_pitch_to_note(self, wrapped_pitch: int):
         return self.DEFAULT_WRAPPED_PITCH_TO_NOTE_MAPPING[wrapped_pitch]
+
+
+class ConvolutionalScaleDetector:
+    def __init__(self):
+        self.chord_type_to_row_num_map = {
+            ChordType.MAJOR: 0,
+            ChordType.SEVENTH: 0,
+            ChordType.MINOR: 1,
+            ChordType.MIN_SEVENTH: 1,
+            ChordType.DIMINISHED: 2,
+            ChordType.DIM_SEVENTH: 2,
+        }
+        # Rows indices represent chord type, column indices represent chord roots in incrementing by
+        # semitone starting at A=0.  These values are populated by my personal experience as a classical
+        # musicion; basically this is machine learning in the sense that I told the machine what to think
+        # and it learned to obey me.
+        KERNEL_TEMPLATE_A_MAJOR = np.array(
+            [
+                [1, -1, 0, -1, 1, 1, -1, 1, -1, 0, -1, 0],  # maj
+                [-1, -1, 1, -1, 1, 0, -1, 0, -1, 1, -1, 0],  # min
+                [-1, -1, 1, -1, -1, 1, -1, -1, 1, -1, -1, 1],  # dim
+            ]
+        )
+        # Circular convolution, each time shifting the kernel to the right.  Each shifted version
+        # of the kernel gets stacked, such that we now have 12x kernels, each representing one
+        # tonal center.
+        self.kernels_3d = np.stack(
+            [np.roll(KERNEL_TEMPLATE_A_MAJOR, shift=i, axis=1) for i in range(12)],
+            axis=0,
+        )
+        # This is the array that will get modified as chords are inserted and removed:
+        self.input_chord_data = np.zeros(shape=(3, 12), dtype=np.int8)
+
+    def insert_chord(self, chord: ScaleAgnosticChord):
+        row_num = self.chord_type_to_row_num_map[chord.chord_type]
+        self.input_chord_data[row_num][chord.root_wrapped_pitch] += 1
+
+    def remove_chord(self, chord: ScaleAgnosticChord):
+        row_num = self.chord_type_to_row_num_map[chord.chord_type]
+        current_value_at_index = self.input_chord_data[row_num][
+            chord.root_wrapped_pitch
+        ]
+        self.input_chord_data[row_num][chord.root_wrapped_pitch] = max(
+            0, current_value_at_index - 1
+        )
+
+    def predict_tonal_center(self):
+        prediction_vector = np.sum(self.kernels_3d * self.input_chord_data, axis=(1, 2))
+        return np.argmax(prediction_vector)
---
+++ test/test_convolutional_scale_detector.py
@@ -0,0 +1,89 @@
+import pytest
+
+from harmony_domain import ScaleAgnosticChord, ChordType
+from scale_analyzer import ConvolutionalScaleDetector
+
+
+class TestConvolutionalScaleDetector:
+    @pytest.fixture(autouse=True)
+    def before_each_test(self):
+        self.patient = ConvolutionalScaleDetector()
+
+    def test_can_predict_obviously_c_major_progression(self):
+        chord_progression = self.very_obviously_c_major_progression()
+        for chord in chord_progression:
+            self.patient.insert_chord(chord)
+        expected_tonal_center = 3  # C major
+
+        actual_tonal_center = self.patient.predict_tonal_center()
+
+        assert actual_tonal_center == expected_tonal_center
+
+    def test_can_change_tonal_center(self):
+        c_major_progression = self.very_obviously_c_major_progression()
+        for chord in c_major_progression:
+            self.patient.insert_chord(chord)
+        for chord in c_major_progression:
+            self.patient.remove_chord(chord)
+        d_major_progression = self.very_obviously_d_major_progression()
+        for chord in d_major_progression:
+            self.patient.insert_chord(chord)
+
+        expected_tonal_center = 5  # D major
+
+        actual_tonal_center = self.patient.predict_tonal_center()
+
+        assert actual_tonal_center == expected_tonal_center
+
+    def test_removing_more_chords_than_present_is_benign(self):
+        c_major_progression = self.very_obviously_c_major_progression()
+        for chord in c_major_progression:
+            self.patient.remove_chord(chord)
+            self.patient.remove_chord(chord)
+            self.patient.insert_chord(chord)
+        expected_tonal_center = 3  # C major
+
+        actual_tonal_center = self.patient.predict_tonal_center()
+
+        assert actual_tonal_center == expected_tonal_center
+
+    # Helpers
+    def very_obviously_c_major_progression(self):
+        return [
+            ScaleAgnosticChord(
+                root_wrapped_pitch=2, chord_type=ChordType.DIM_SEVENTH
+            ),  # B dim7
+            ScaleAgnosticChord(
+                root_wrapped_pitch=7, chord_type=ChordType.SEVENTH
+            ),  # E 7
+            ScaleAgnosticChord(
+                root_wrapped_pitch=0, chord_type=ChordType.MINOR
+            ),  # A min
+            ScaleAgnosticChord(
+                root_wrapped_pitch=5, chord_type=ChordType.MINOR
+            ),  # D min
+            ScaleAgnosticChord(
+                root_wrapped_pitch=10, chord_type=ChordType.SEVENTH
+            ),  # G 7
+            ScaleAgnosticChord(root_wrapped_pitch=3, chord_type=ChordType.MAJOR),  # C
+        ]
+
+    def very_obviously_d_major_progression(self):
+        return [
+            ScaleAgnosticChord(
+                root_wrapped_pitch=4, chord_type=ChordType.DIM_SEVENTH
+            ),  # C# dim7
+            ScaleAgnosticChord(
+                root_wrapped_pitch=9, chord_type=ChordType.SEVENTH
+            ),  # F# 7
+            ScaleAgnosticChord(
+                root_wrapped_pitch=2, chord_type=ChordType.MINOR
+            ),  # B min
+            ScaleAgnosticChord(
+                root_wrapped_pitch=7, chord_type=ChordType.MINOR
+            ),  # E min
+            ScaleAgnosticChord(
+                root_wrapped_pitch=0, chord_type=ChordType.SEVENTH
+            ),  # A 7
+            ScaleAgnosticChord(root_wrapped_pitch=5, chord_type=ChordType.MAJOR),  # D
+        ]
